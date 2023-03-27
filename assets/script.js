c2a84ef88575c3454aeed944d10b682f26b8b881
jkrieger6 <jkrieger11@gmail.com>
2023-03-27 11:32:58 -0700

    added code to try and get weather to appear in js

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,20 @@ var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={searchLocation}&l
 var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon={lon}&units=imperial&appid={APIKey}"
 
 fetch(weatherUrl)
-.then(response => response.json())
-.then(data => {
-    var forecastData = data.list;
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    for (let i = 0; i < data.length; i++) {
+        var forecastData = data.list;
+        
+    }
+    // var forecastData = data.list;
 
     forecastDataforEach(item => {
         var date = new Date(item.dt_txt);
         var time = date.toLocaleTimeString();
         var temp = item.main.temp;
         var description = item.weather[0].description;
-  
+    })
 })
