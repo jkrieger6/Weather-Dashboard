dbb20258aa02477debbd619c90521598434ee4bd
jkrieger6 <jkrieger11@gmail.com>
2023-03-27 11:38:36 -0700

    added new code to get weather to appear

--- assets/script.js
+++ assets/script.js
@@ -7,18 +7,15 @@ var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon
 fetch(weatherUrl)
 .then(function (response) {
     return response.json();
-})
-.then(function (data) {
-    for (let i = 0; i < data.length; i++) {
-        var forecastData = data.list;
-        
-    }
-    // var forecastData = data.list;
-
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+    
+  var forecastData = data.list;
     forecastDataforEach(item => {
         var date = new Date(item.dt_txt);
         var time = date.toLocaleTimeString();
         var temp = item.main.temp;
         var description = item.weather[0].description;
     })
-})
