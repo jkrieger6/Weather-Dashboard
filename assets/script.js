36f2ba1c73416761136233617f7e805727e7a211
jkrieger6 <jkrieger11@gmail.com>
2023-03-28 15:54:13 -0700

    got geo fetch to work and added functions for search

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,94 @@
 var APIKey = "5b2b7b9a047f3cbe2fa1edd5d1203608";
-var userInputEl = document.getElementById("userInput");
-var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={searchLocation}&limit=5&appid=5b2b7b9a047f3cbe2fa1edd5d1203608'
-var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon={lon}&units=imperial&appid=5b2b7b9a047f3cbe2fa1edd5d1203608"
-var userFormEl = document.querySelector('#user-form');
+var $userInputEl = $("#userInput");
+
+var weatherUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon={lon}&units=imperial&appid=5b2b7b9a047f3cbe2fa1edd5d1203608";
+var userFormEl = document.querySelector("#user-form");
 // collects users input in the search and displays it into the search history
 function addResult() {
-    cityInput = document.getElementById("userInput").value;
-    searchHistoryEl = getInfo();
-    var searchCity = $("<div>")
-    searchCity.attr('id', cityInput)
-    searchCity.text(cityInput)
-    searchCity.addClass("h4")
+  var cityInput = $userInputEl.val();
+  // searchHistoryEl = getInfo();
+  var searchCity = $("<div>");
+  searchCity.attr("id", cityInput);
+  searchCity.text(cityInput);
+  searchCity.addClass("h4");
 
-    if (searchHistoryEl.includes(cityInput) == false) {
-        getElementById("search-history").append(searchCity)
-    }
-   getElementById("current-weather").attr("style", "display:inline-block")
-   addInfo(cityInput);
-};
-
-function getWeather() {
-
+  // if (!searchHistoryEl.includes(cityInput)) {
+  //     $("#search-history").append(searchCity)
+  // }
+  $("#current-weather").attr("style", "display:inline-block");
+  getCityData(cityInput);
 }
+
+function getCityData(city) {
+  var geoUrl =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    "&limit=5&appid=" +
+    APIKey;
+  fetch(geoUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var cityData = data[0];
+      console.log(cityData);
+      var geoLatitude = cityData.lat;
+      var geoLongitude = cityData.lon;
+      getWeatherData(geoLatitude, geoLongitude);
+    });
+}
+
+
 // add event listener to search history buttons
-document.getElementById('searchBtn').on("click", addResult);
-document.getElementById('searchBtn').on("click", getWeather);
+$("#searchBtn").on("click", addResult);
+// $('#searchBtn').on("click", getWeather);
 
 //add event listener to seach history items
-document.getElementById("search-history").on('click', function(event) {
-event.preventDefault();
-document.getElementById("current-weather").attr('style', "display:inline-block")
-userInputEl.value = event.target.id;
-getResult();
+$("search-history").on("click", function (event) {
+  event.preventDefault();
+  $("current-weather").attr("style", "display:inline-block");
+  userInputEl.value = event.target.id;
+  getResult();
 });
 
-function getResult() {
-    var countryCode = "US";
-    var cityCode = cityInput;
-    var geoLongitude;
-    var geoLatitude;
-    var cityName = ("<h>");
-    cityName.addClass("h3");
-    var temperature = $("<div>");
-    var humidity = $("<div>")
-    var windSpeed = ("<div>");
-    var weatherIcon = ("<img>");
-    weatherIcon.addClass("icon");
-    var dateTime =("<div>");
+function getWeatherData(lat, lon) {
 
-    $(".forecast-five-day").empty();
-    $(".city").empty();
-    cityInput = document.getElementById('userInput').value;
+  var $cityName = $("<h3>");
+  var temperature = $("<div>");
+  var humidity = $("<div>");
+  var windSpeed = $("<div>");
+  var weatherIcon = $("<img>");
+  weatherIcon.addClass("icon");
+  var dateTime = "<div>";
 
-    $(".city").addClass("list-group");
-    $(".city").append(cityName);
-    $(".city").append(dateTime);
-    $(".city").append(weatherIcon);
-    $(".city").append(temperature);
-    $(".city").append(windSpeed);
-    $(".city").append(humidity);
+  $(".forecast-five-day").empty();
+  $(".city").empty();
+  cityInput = $("userInput").value;
 
+  $(".city").addClass("list-group");
+  $(".city").append($cityName);
+  $(".city").append(dateTime);
+  $(".city").append(weatherIcon);
+  $(".city").append(temperature);
+  $(".city").append(windSpeed);
+  $(".city").append(humidity);
 
-    fetch(weatherUrl)
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-    
-  var forecastData = data.list;
-    forecastDataforEach(item => {
-        var date = new Date(item.dt_txt);
-        var time = date.toLocaleTimeString();
-        var temp = item.main.temp;
-        var description = item.weather[0].description;
+var weatherUrl =
+  "https://api.openweathermap.org/data/2.5/forecast?lat="+ lat + "&lon=" + lon + "&units=imperial&appid="+ APIKey;
+  fetch(weatherUrl)
+    .then(function (response) {
+      return response.json();
     })
-    
+    .then(function (data) {
+      console.log(data);
+    });
+
+  // var forecastData = data.list;
+  // forecastDataforEach((item) => {
+  //   var date = new Date(item.dt_txt);
+  //   var time = date.toLocaleTimeString();
+  //   var temp = item.main.temp;
+  //   var description = item.weather[0].description;
+  // });
 }
