03f0c061ecda54594f86a4046ce9cbbe820c9685
jkrieger6 <jkrieger11@gmail.com>
2023-03-27 18:29:14 -0700

    added fetch and url to js

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,18 @@ var state;
 var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={searchLocation}&limit=5&appid={APIKey}'
 var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon={lon}&units=imperial&appid={APIKey}"
 
+var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q={searchLocation}&limit=5&appid=5b2b7b9a047f3cbe2fa1edd5d1203608'
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=$lat}&lon={lon}&units=imperial&appid=5b2b7b9a047f3cbe2fa1edd5d1203608"
+var userFormEl = document.querySelector('#user-form');
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    var cityInput = city.value.trim();
+    if (cityInput) {
+        getCity(cityInput);
+        searchHistoryEl
+    }
+}
 fetch(weatherUrl)
 .then(function (response) {
     return response.json();
